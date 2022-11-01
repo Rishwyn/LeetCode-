5af7900d6e779b2d43a523eb69d0a32d5dc27e58
Antony Rishwyn <116724735+Antony-Rishwyn@users.noreply.github.com>
2022-11-01 17:45:43 +0530

    Create Climbing Stairs.cpp

---
+++ Climbing Stairs.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int climbStairs(int n) {
+        if(n==1)
+            return 1;
+        else if(n==2)
+            return 2;
+        int f=1,s=2,i=3,t;
+        while(i<=n)
+        {
+            i++;
+            t=f+s;
+            f=s;
+            s=t;
+        }
+        return t;
+    }
+};
