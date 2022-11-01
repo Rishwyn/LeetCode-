1ac754fa41bed3634c7cb7cff3feb7def219ad8b
Antony Rishwyn <116724735+Antony-Rishwyn@users.noreply.github.com>
2022-11-01 17:42:10 +0530

    Create Reverse Integer.cpp

---
+++ Reverse Integer.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int reverse(int x) {
+         long long r=0;
+        while(x)
+        {
+            r=r*10+x%10;
+            x/=10;
+        }
+        return (int(r)==r)*r;
+         }
+    
+};
